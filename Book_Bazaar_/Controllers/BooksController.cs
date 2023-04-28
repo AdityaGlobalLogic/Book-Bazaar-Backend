001d7c72caf4f484f6b521bd1e739e620034da6e
Vishvajeet Singh <vishvajeet.singh2@globallogic.com>
2023-04-28 12:07:22 +0530

    books list added

--- Book_Bazaar_/Controllers/BooksController.cs
+++ Book_Bazaar_/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Book_Bazaar_.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace Book_Bazaar_.Controllers
 {
@@ -7,5 +9,52 @@ namespace Book_Bazaar_.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        
+        private readonly IConfiguration _configuration;
+        public BooksController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("api/books")]
+        public async  Task<ActionResult> GetBooks()
+        {
+            List<Books> books = new List<Books>();
+
+            using (SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("MyCon").ToString()))
+            {
+                connection.Open();
+
+                // Retrieve list of books from database
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Books", connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Books book = new Books
+                            {
+                                BookID = (int)reader["BookID"],
+                                Title = (string)reader["Title"],
+                                Description = (string)reader["Description"],
+                                AuthorName = (string)reader["AuthorName"],
+                                Price = (decimal)reader["Price"],
+                                Quantity= (int)reader["Quantity"],
+                                ISBN = (int)reader["ISBN"],
+                                BookImage = (string)reader["BookImage"],
+                                UserID = (int)reader["UserID"],
+                                CategoryID = (int)reader["CategoryID"]
+                            };
+
+                            books.Add(book);
+                        }
+                    }
+                }
+            }
+
+            return Ok(books);
+        }
     }
+}
 }
